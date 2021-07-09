f77fae29d8da3416a90c935fa69dedbcc96b403a
bfranceschin <bfranceschin@gmail.com>
2021-07-09 12:56:52 -0300

    class properties

--- src/App.js
+++ src/App.js
@@ -6,45 +6,33 @@ import ExchangeHeader from './components/ExchangeHeader/ExchangeHeader';
 //import {v4 as uuidv4} from 'uuid';
 
 export default class App extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      balance: 10000,
-      coinData: [
-        {name: "Bitcoin",      ticker: "BTC",  price: 60000, balance: 0.5},
-        {name: "Ethereum",     ticker: "ETH",  price: 5000, balance: 4.5},
-        {name: "Tether",       ticker: "USDT", price: 1.000, balance: 1000},
-        {name: "Ripple",       ticker: "XRP",  price: 0.2, balance: 0},
-        {name: "Bitcoin Cash", ticker: "BCH",  price: 612.0, balance: 0},
-      ],
-      showBalance: false,
-    }
-    this.handleRefresh = this.handleRefresh.bind(this);
-    this.handleShowBalance = this.handleShowBalance.bind(this);
+  state = {
+    balance: 10000,
+    coinData: [
+      {name: "Bitcoin",      ticker: "BTC",  price: 60000, balance: 0.5},
+      {name: "Ethereum",     ticker: "ETH",  price: 5000, balance: 4.5},
+      {name: "Tether",       ticker: "USDT", price: 1.000, balance: 1000},
+      {name: "Ripple",       ticker: "XRP",  price: 0.2, balance: 0},
+      {name: "Bitcoin Cash", ticker: "BCH",  price: 612.0, balance: 0},
+    ],
+    showBalance: false,
   }
-
-  handleRefresh (changeValueTicker)
+  handleRefresh = (changeValueTicker) =>
   {
     // const coin = this.state.coinData.find(c => c.ticker === changeValueTicker);
     // console.log(coin);
-    let refreshedData = this.state.coinData.map( function({ticker, price, name, key, balance}) {
-      let newPrice = price;
-      if (ticker === changeValueTicker) {
+    let refreshedData = this.state.coinData.map( function(values) {
+      let newValues = {...values};
+      if (newValues.ticker === changeValueTicker) {
         const randomPercentage = 0.995 + Math.random() * 0.01;
-        newPrice = price * randomPercentage;
+        newValues.price *= randomPercentage;
       }
-      return {
-        name,
-        key,
-        ticker,
-        price: newPrice,
-        balance,
-      };
+      return newValues;
     })
     this.setState({coinData: refreshedData});
   }
 
-  handleShowBalance ()
+  handleShowBalance = () =>
   {
     let newShowBalance = !this.state.showBalance;
     this.setState({showBalance: newShowBalance});
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -10,12 +10,7 @@ const CoinRow = styled.tr `
 `;
 
 export default class Coin extends Component {
-    constructor (props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick (event) {
+    handleClick = (event) => {
         event.preventDefault();
         this.props.handleRefresh(this.props.ticker);
     }
